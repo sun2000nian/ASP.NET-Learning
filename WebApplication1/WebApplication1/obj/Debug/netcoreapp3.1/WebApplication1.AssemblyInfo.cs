51e8ccce96c9d94e63b847872e9da1b3644b0731
孙昊哲 <sun2000nian@live.com>
2020-07-16 23:07:36 +0800

    0

--- WebApplication1/WebApplication1/obj/Debug/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/netcoreapp3.1/WebApplication1.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("327db45d-0668-4774-8da0-a9da129c1f85")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
